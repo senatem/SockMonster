6d73196ad92b832f6548d996a51cd929a4d54f72
celik97 <i.celik97@gmail.com>
2021-01-30 22:57:11 +0300

    font, galley start

--- core/src/com/pungo/game/scenes/GalleryScene.kt
+++ core/src/com/pungo/game/scenes/GalleryScene.kt
@@ -1,6 +1,14 @@
 package com.pungo.game.scenes
 
+import com.pungo.modules.basic.geometry.Rectangle
 import com.pungo.modules.scenes.Scene
 
 class GalleryScene: Scene("gallery",0f) {
+
+    init {
+
+        mainDistrict.addFullPlot("back", Rectangle(44f / 1280f, 214f / 1280f, 616f / 720f, 702f / 720f))
+
+    }
+
 }
